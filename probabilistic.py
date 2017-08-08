947f90b107e30a291525134940f39f78696ee08d
MeccaLeccaHi <ajones173@gmail.com>
2017-08-08 09:32:29 -0700

    finishing implementing probabilistic objects

--- probabilistic.py
+++ probabilistic.py
@@ -20,47 +20,32 @@ class Probabilistic(linkage_tools.Linker):
 		self.activation_function = lambda x: np.sum(np.log(x))
 		
 	# Train the probabilistic linker
-	def train(self, inputs_list, targets_list, input_ids):
-		'''e.g. pr.train(bool_table[feature_vals],bool_table['real_match'],bool_table[['newb_id','bc_id']])'''
+	def train(self, inputs_list, winner_index):
+		'''e.g. pr.train(bool_table[feature_vals],bool_table['pair_match'])'''
 
 		# Convert inputs list to 2d array
 		inputs = np.array(inputs_list, ndmin=2)
-		targets = np.array(targets, ndmin=2)
-
-		# Calculate probabilities going into linker
-		mprob_mat = np.tile(self.m_probs, inputs.shape)
-		mprob_mat[inputs==0] = 1 - mprob_mat[inputs==0]
-		uprob_mat = np.tile(self.u_probs, inputs.shape)
-		uprob_mat[inputs==0] = 1 - uprob_mat[inputs==0]
-
-		# Calculate the probabilistic linker predictions
-		linkage_score = np.apply_along_axis(self.activation_function, 1, foo_probs/bar_probs)
-		# Convert to linkage score to linkage 'cost'
-		linkage_cost = abs(linkage_score - linkage_score.max())
-
-		# Maximize pair-wise linkage scores (minimize cost with Munkres)
-		link_list = input_ids.assign(linkage_cost = linkage_cost)
-		winner_index = self.maximize(link_list)
 		
-		mask = np.ones(len(link_list), np.bool)
-		mask[winner_index] = 0
-		loser_index = np.where(mask)
-
+		loser_index = np.ones(len(inputs_list), np.bool)
+		loser_index[winner_index] = 0
+		
 		# Count number of matches for each field
-		u_match = sum(inputs[loser_index,:]) 
-		m_match = sum(inputs[winner_index,:]) 
+		u_match = [sum(i) for i in zip(*inputs[np.where(loser_index)])]
+		m_match = [sum(i) for i in zip(*inputs[np.where(winner_index)])]
 
 		# Count total number of pairs
-		u_pair = sum(inputs==False)
-		m_pair = sum(inputs==True)
+		u_pair = np.sum(loser_index)
+		m_pair = np.sum(winner_index)
 
 		# Update m- and u-probabilities
-		self.u_probs = np.divide(u_match_count,u_pair_count)
-		self.m_probs = np.divide(m_match_count,m_pair_count)
+		self.m_probs = np.true_divide(m_match,m_pair)
+		self.u_probs = np.true_divide(u_match,u_pair)
+
+		print self.m_probs,self.u_probs
 
 	# Query the probabilistic linker
 	def query(self, inputs_list, input_ids):
-		'''e.g. pr.query(bool_table[feature_vals])'''
+		'''e.g. pr.query(bool_table[feature_vals],bool_table[['newb_id','bc_id']])'''
 
 		# Convert inputs list to 2d array
 		inputs = np.array(inputs_list, ndmin=2)
